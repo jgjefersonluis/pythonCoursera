680f87a4a05de55bbd5262cc5732e0f5228bf779
Jeferson Luis <jgjefersonluis@gmail.com>
2021-11-03 02:56:22 -0300

    (Semana 1 - arquivo organizado em pasta - status concluído)

---
+++ semana1/comandoPrint.py
@@ -0,0 +1,9 @@
+print("Olá Mundo! Bem-vindo ao curso de Introdução à Ciência da Computação em Python.")
+print("Meu primeiro programa soma os numeros 2 e 3:")
+print(2 + 3)
+
+a= 2+3*4
+b = 21 == 3*7
+
+print(a)
+print(b)
---
+++ semana1/variaveisEprgs.py
@@ -0,0 +1,16 @@
+soma = 5.5
+print("soma=", soma)
+
+x=10
+y=30
+x=x+10
+y=x+10
+x=x+y
+
+print(x)
+
+a = 1
+
+b = 2
+
+print (a + 2 * b)
