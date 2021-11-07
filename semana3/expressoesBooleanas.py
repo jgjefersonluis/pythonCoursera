f8256af93860fa75f3d68c54e5e3a531fe7bbb11
Jeferson Luis <jgjefersonluis@gmail.com>
2021-11-07 01:36:12 -0300

    expressões booleanas

---
+++ semana3/expressoesBooleanas.py
@@ -0,0 +1,39 @@
+5>3
+18==9*2
+x=12312
+x<0
+type(False)
+
+x > 0 and x ** 1 > 100
+
+x < 0 or x == 12312
+
+not x > 0
+
+not False
+
+not True
+
+not not True
+
+fizerSol = True
+forFeriado = False
+vouParaPraia = fizerSol and forFeriado
+vouParaPraia
+
+forFeriado = True
+vouParaPraia = fizerSol and forFeriado
+vouParaPraia
+
+paitrocinio = False
+rolarPromocao = True
+vouAoShow = paitrocinio or rolarPromocao
+vouAoShow
+
+((x>0) and (not y == 50)) or (x+y == 150)
+
+
+
+
+
+
