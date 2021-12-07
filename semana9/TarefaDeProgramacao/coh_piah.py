20a88e4df975bed68d77c0f92e49ebdca198c3de
Jeferson Luis <jgjefersonluis@gmail.com>
2021-12-07 02:47:22 -0300

    programa final coh-piah || iniciando o projeto função separa_palavras e palavras unicas.

--- semana9/TarefaDeProgramacao/coh_piah.py
+++ semana9/TarefaDeProgramacao/coh_piah.py
@@ -41,7 +41,21 @@ def separa_palavras(frase):
     '''A função recebe uma frase e devolve uma lista das palavras dentro da frase'''
     return frase.split()
 
+def n_palavras_unicas(lista_palavras):
+    '''Essa funcao recebe uma lista de palavras e devolve o numero de palavras que aparecem uma unica vez'''
+    freq = dict()
+    unicas = 0
+    for palavra in lista_palavras:
+        p = palavra.lower()
+        if p in freq:
+            if freq[p] == 1:
+                unicas -= 1
+            freq[p] += 1
+        else:
+            freq[p] = 1
+            unicas += 1
 
+    return unicas
 
 
 
