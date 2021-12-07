b8a33d716fbab47963c87c8e2b8f3d0b4f4256ba
Jeferson Luis <jgjefersonluis@gmail.com>
2021-12-07 02:28:56 -0300

    programa final coh-piah || iniciando o projeto função le_textos.

--- semana9/TarefaDeProgramacao/coh_piah.py
+++ semana9/TarefaDeProgramacao/coh_piah.py
@@ -6,16 +6,66 @@ def le_assinatura():
     ser comparada com os textos forneciodos'''
     print("Bem vindo ao detector automático de COH-PIAH.")
 
-    wal = float(input("Entre o tamanho medio de palavra: "))
-    ttr = float(input("Entre a relação Type-Token: "))
+    wal=float(input("Entre o tamanho medio de palavra: "))
+    ttr=float(input("Entre a relação Type-Token: "))
+    hlr=float(input("Entre a Razão Hapax Legomana: "))
+    sal=float(input("Entre o tamanho médio de sentença: "))
+    sac=float(input("Entre a complexidade média da sentença: "))
+    pal=float(input("Entre o tamanho medio de frase: "))
 
-    hlr = float(input("Entre a Razão Hapax Legomana: "))
-    sal = float(input("Entre o tamanho médio de sentença: "))
-
-    sac = float(input("Entre a complexidade média da sentença: "))
-    pal = float(input("Entre o tamanho medio de frase: "))
 
     return [wal, ttr, hlr, sal, sac, pal]
+
+
+def le_textos():
+    i=1
+    textos=[]
+    texto=input("Digite o texto"+str(i)+"(aperte enter para sair):")
+    while texto:
+        textos.append(texto)
+        i+=1
+        texto=input("Digite o texto"+str(i)+"(aperte enter para sair):")
+
+
+    return textos
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
                        
 
 
